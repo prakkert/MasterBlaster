ca0f250c0f664886b1b38445443df21ad6426669
hellewegenRebers <d.hellewegen@gmail.com>
2013-11-11 17:26:08 +0100

    Added new engine project.

---
+++ MasterBlaster/EventProviders/BaseEventProvider.cs
@@ -0,0 +1,25 @@
+﻿using MasterBlaster.Components;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasterBlaster.EventProviders
+{
+    public abstract class BaseEventProvider : IUpdatableComponent
+    {
+        public Guid Id { get; private set; }
+        public bool Destroyed { get; set; }
+
+        public BaseEventProvider()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MasterBlaster/EventProviders/CollisionEventProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasterBlaster.EventProviders
+{
+    public class CollisionEventProvider : BaseEventProvider
+    {
+        public CollisionEventProvider()
+            : base()
+        {
+        }
+    }
+}
